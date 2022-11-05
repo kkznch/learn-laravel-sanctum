8f6e974573ddb4fbeb5baad80e6ec5a2eb20761f
kkznch <kkznch710md@gmail.com>
2022-11-05 21:46:52 +0900

    ThreadとResponse返すようにした

--- backend/app/Models/Thread.php
+++ backend/app/Models/Thread.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Thread extends Model
@@ -23,5 +24,13 @@ class Thread extends Model
     public function responses(): HasMany
     {
         return $this->hasMany(Response::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
